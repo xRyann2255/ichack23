01caaca6cbd5116cb9f12e94562bcb1127d5a018
rv622 <rv622@ic.ac.uk>
2023-02-04 16:19:05 +0000

    amazon ip ranges

--- client/getData.py
+++ client/getData.py
@@ -1,5 +1,8 @@
-import requests
+import json, pathlib
+HOME = pathlib.Path(__file__).parent.absolute()
 
-def getInfo(call):
-    r = requests.get(call)
-    return r.json()
+with open(HOME/'../assets/ip-ranges/amazon-ip-ranges.json', 'r') as file:
+    data = json.load(file)
+
+amazon_ip_ranges = [prefix['ip_prefix'] for prefix in data['prefixes']]
+print(amazon_ip_ranges)

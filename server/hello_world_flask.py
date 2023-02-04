867489790bb5ef2d51c02a68002b8cb9ec1eabd3
Alex <noelalexandre66@gmail.com>
2023-02-04 22:26:32 +0000

    Pytricia update, remember to install req.txt

--- server/hello_world_flask.py
+++ server/hello_world_flask.py
@@ -1,5 +1,6 @@
 import db_tools as tools
-import dotenv, sqlite3, pathlib, requests
+import pytricia
+import dotenv, sqlite3, pathlib, requests, json
 from os import getenv
 from flask import Flask, request
 from operator import itemgetter
@@ -13,30 +14,39 @@ db = sqlite3.connect(HOME/'test.db')
 app = Flask(__name__)
 api = Api(app)
 
+pyt = pytricia.PyTricia()
+for ip,co2 in json.load(open(HOME/'../assets/all-ranges.json')).items():
+    pyt[ip] = co2
+
+# print(pyt.get('3.2.34.1'))
+
 def get_url_data(url:str):
-    print('started')
-    resp = requests.post(f'https://www.klazify.com/api/categorize?url={url}',headers={'Authorization':f'Bearer {getenv("KTOKEN")}','User-Agent':'telnet'})
-    print(resp)
+    # print('started')
+    resp = requests.post(f'https://www.klazify.com/api/categorize?url={url}',headers={'Authorization':f'Bearer {getenv("KTOKEN")}','User-Agent':'carbonara_api'})
+    # print(resp)
     data = resp.json()
-    print(data)
-    return dict(
-        logo=data["domain"]["logo_url"],
-        category=data["domain"]["categories"][0]["name"],
+    print(f"{data = }")
+    return (
+        data["domain"]["logo_url"],
+        data["domain"]["categories"][0]["IAB12"],
     )
 
 
-class TodoSimple(Resource):
+class Rest(Resource):
     def get(self, name):
         return {}
 
     def put(self, name):
-        name,url,time,co2 = (request.form[n] for n in ["name","url","timestamp","co2_emissions"])
-        data = get_url_data(url)
-        print(name,time,url,data["category"],co2,data["logo"])
-        # tools.update_row(name,time,url,data["category"],co2,data["logo"])
+        print(request.json)
+        data = request.json["hosts"]
+        co2 = sum(pyt.get(d["ip"])*d["transferred"] for d in data.values()) # fix ratio :/
+        url,time = request.json["url"],request.json["timestamp"]
+        logo,category = get_url_data(url)
+        print(name,time,url,category,co2,logo)
+        # tools.update_row(name,time,url,category,co2,logo)
         return {}
 
-api.add_resource(TodoSimple, '/<string:name>')
+api.add_resource(Rest, '/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)

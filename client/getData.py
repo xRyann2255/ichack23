2c5b326b940da1fa098327e8a063aaf7f2a2ace9
Alex <noelalexandre66@gmail.com>
2023-02-04 16:40:41 +0000

    getData modified

--- client/getData.py
+++ client/getData.py
@@ -1,8 +1,16 @@
 import json, pathlib
 HOME = pathlib.Path(__file__).parent.absolute()
 
-with open(HOME/'../assets/ip-ranges/amazon-ip-ranges.json', 'r') as file:
-    data = json.load(file)
+all_ranges = {}
+services = {'amazon':'aws','google':'gcp','azure':'azure'}
+ip_fmt = {'amazon':'ip_prefix','google':'ipv4Prefix','azure':'addressPrefix'}
+scope_fmt = {'aws':'region','gcp':'scope','azure':''}
 
-amazon_ip_ranges = {prefix["ip_prefix"]: prefix["region"] for prefix in data["prefixes"]}
-print(amazon_ip_ranges)
+def get_co2(service,scope):
+    ...
+
+
+for name in ['amazon','google']:
+    with open(HOME/f'../assets/ip-ranges/{name}-ip-ranges.json', 'r') as file:
+        data = json.load(file)
+        all_ranges.update({prefix[ip_fmt[name]]: get_co2(services[name],prefix[scope_fmt[name]]) for prefix in data["prefixes"]})
--- server/hello_world_flask.py
+++ server/hello_world_flask.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+todos = {}
 
-api.add_resource(HelloWorld, '/')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        print(todo_id)
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)

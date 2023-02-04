dc8abf2efb24e91abe372d9db62b59833bb85f14
rv622 <rv622@ic.ac.uk>
2023-02-04 18:25:16 +0000

    db tools started update

--- server/db_tools.py
+++ server/db_tools.py
@@ -6,16 +6,10 @@ HOME = pathlib.Path(__file__).parent.absolute()
 
 db = sqlite3.connect(HOME/'test.db')
 
-c = db.cursor()
-c.execute(
-"""
-CREATE TABLE user_template (
-  timestamp TIMESTAMP NOT NULL,
-  website VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL,
-  co2_emissions FLOAT NOT NULL,
-  logo_url VARCHAR(255) NOT NULL
-);
-"""
-)
+def update_row(name, timestamp, website, category, co2_emissions, logo_url):
+    c = db.cursor()
+    c.execute(
+    
+
+
 

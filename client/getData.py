9887a3424a0ce11e5c2178dd5723d3603c532a31
rv622 <rv622@ic.ac.uk>
2023-02-04 16:24:18 +0000

    dictionary amazon

--- client/getData.py
+++ client/getData.py
@@ -4,5 +4,5 @@ HOME = pathlib.Path(__file__).parent.absolute()
 with open(HOME/'../assets/ip-ranges/amazon-ip-ranges.json', 'r') as file:
     data = json.load(file)
 
-amazon_ip_ranges = [prefix['ip_prefix'] for prefix in data['prefixes']]
+amazon_ip_ranges = {prefix["ip_prefix"]: prefix["region"] for prefix in data["prefixes"]}
 print(amazon_ip_ranges)

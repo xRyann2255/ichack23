40206ceb4eda6e3c5ce3e0db8363801dc0a9844e
Alex <noelalexandre66@gmail.com>
2023-02-04 20:19:22 +0000

    various stuff

--- server/db_tools.py
+++ server/db_tools.py
@@ -25,19 +25,7 @@ INSERT INTO main.{name} (timestamp, website, category, co2_emissions, logo_url)
 VALUES ('{timestamp}', '{website}', '{category}', '{co2_emissions}', '{logo_url}');
 """
 )
-
-update_row('Ryan', '2023-02-04 12:00:00', 'www.example.com', 'Transportation', 50, 'www.example.com/logo.png')
-update_row('Ryan', '2023-02-05 12:00:00', 'www.example.com', 'Food', 30, 'www.example.com/logo.png')
-update_row('Ryan', '2023-02-06 12:00:00', 'www.anothersite.com', 'Electricity', 40, 'www.anothersite.com/logo.png')
-update_row('Ryan', '2023-02-07 12:00:00', 'www.yetanotherexample.com', 'Transportation', 60, 'www.yetanotherexample.com/logo.png')
-update_row('Ryan', '2023-02-08 12:00:00', 'www.example.com', 'Food', 25, 'www.example.com/logo.png')
-update_row('Jim', '2023-02-09 12:00:00', 'www.anothersite.com', 'Electricity', 35, 'www.anothersite.com/logo.png')
-update_row('Jim', '2023-02-06 12:00:00', 'www.anothersite.com', 'Electricity', 40, 'www.anothersite.com/logo.png')
-update_row('Alex', '2023-02-07 12:00:00', 'www.yetanotherexample.com', 'Transportation', 60, 'www.yetanotherexample.com/logo.png')
-update_row('Alex', '2023-02-08 12:00:00', 'www.example.com', 'Food', 25, 'www.example.com/logo.png')
-update_row('Jack', '2023-02-09 12:00:00', 'www.anothersite.com', 'Electricity', 35, 'www.anothersite.com/logo.png')
-
-
+    
 def get_websites(name):
     return db.execute(f"SELECT website, logo_URL, category, co2_emissions FROM {name} ORDER BY timestamp")
 
@@ -45,3 +33,14 @@ def data_points(name):
     points = db.execute(f"SELECT timestamp, co2_emissions FROM {name} ORDER BY timestamp")
     print(points)
 
+if __name__ == '__main__':
+    update_row('Ryan', '2023-02-04 12:00:00', 'www.example.com', 'Transportation', 50, 'www.example.com/logo.png')
+    update_row('Ryan', '2023-02-05 12:00:00', 'www.example.com', 'Food', 30, 'www.example.com/logo.png')
+    update_row('Ryan', '2023-02-06 12:00:00', 'www.anothersite.com', 'Electricity', 40, 'www.anothersite.com/logo.png')
+    update_row('Ryan', '2023-02-07 12:00:00', 'www.yetanotherexample.com', 'Transportation', 60, 'www.yetanotherexample.com/logo.png')
+    update_row('Ryan', '2023-02-08 12:00:00', 'www.example.com', 'Food', 25, 'www.example.com/logo.png')
+    update_row('Jim', '2023-02-09 12:00:00', 'www.anothersite.com', 'Electricity', 35, 'www.anothersite.com/logo.png')
+    update_row('Jim', '2023-02-06 12:00:00', 'www.anothersite.com', 'Electricity', 40, 'www.anothersite.com/logo.png')
+    update_row('Alex', '2023-02-07 12:00:00', 'www.yetanotherexample.com', 'Transportation', 60, 'www.yetanotherexample.com/logo.png')
+    update_row('Alex', '2023-02-08 12:00:00', 'www.example.com', 'Food', 25, 'www.example.com/logo.png')
+    update_row('Jack', '2023-02-09 12:00:00', 'www.anothersite.com', 'Electricity', 35, 'www.anothersite.com/logo.png')
--- server/hello_world_flask.py
+++ server/hello_world_flask.py
@@ -1,8 +1,12 @@
+import db_tools as tools
+import dotenv, sqlite3, pathlib, requests
+from os import getenv
 from flask import Flask, request
-import sqlite3
+from operator import itemgetter
 from flask_restful import Resource, Api
-import pathlib
+
 HOME = pathlib.Path(__file__).parent.absolute()
+dotenv.load_dotenv(HOME/'.env')
 
 db = sqlite3.connect(HOME/'test.db')
 
@@ -11,16 +15,28 @@ api = Api(app)
 
 todos = {}
 
+def get_url_data(url:str):
+    print('started')
+    data = requests.get(f'https://www.klazify.com/api/categorize?url={url}',headers={'Authorization':f'Bearer {getenv("KTOKEN")}'}).json()
+    print(data)
+    return dict(
+        logo=data["domain"]["logo_url"],
+        category=data["domain"]["categories"][0]["name"],
+    )
+
+
 class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+    def get(self, name):
+        return {}
 
-    def put(self, todo_id):
-        print(todo_id)
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+    def put(self, name):
+        name,url,time,co2 = (request.form[n] for n in ["name","url","timestamp","co2_emissions"])
+        data = get_url_data(url)
+        print(name,time,url,data["category"],co2,data["logo"])
+        # tools.update_row(name,time,url,data["category"],co2,data["logo"])
+        return {}
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(TodoSimple, '/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)

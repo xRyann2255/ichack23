e046b9c1788627b9b96a79b2cc633bdc1d044cc9
jamacz <jack.mccomish@icloud.com>
2023-02-04 20:04:43 +0000

    Add animations and graph

--- client/main.js
+++ client/main.js
@@ -1,28 +1,11 @@
-var categories = [
-    {
-        name: "Streaming", percentage: 0, amount: 0, sites: [
-            {name: "Netflix", percentage: 0, amount: 42.4325},
-            {name: "Amazon Prime", percentage: 0, amount: 24.231742},
-            {name: "Disney+", percentage: 0, amount: 5.241}
-        ]
-    },
-    {
-        name: "Shopping", percentage: 0, amount: 0, sites: [
-            {name: "Amazon", percentage: 0, amount: 18.5235},
-            {name: "Ebay", percentage: 0, amount: 16.1245}
-        ]
-    },
-    {
-        name: "Nothing", percentage: 0, amount: 0, sites: []
-    }
-];
+
 
 function adjustSiteColor(amount, maxAmount) {
     let p = 1 - (amount / maxAmount)
     if (p > 0.5) {
         let r = Math.round(208 + (1 - p) * 2 * 47);
         console.log(r);
-        let rHex =r.toString(16)
+        let rHex = r.toString(16)
         console.log(rHex);
         if (rHex.length == 1) {
             rHex = "0" + rHex;
@@ -64,7 +47,7 @@ function adjustCategoryColor(amount, maxAmount) {
 }
 
 // call after update to `categories` variable, will update the statistics table
-function updateCategories() {
+function updateCategories(categories) {
     let categoriesElement = document.getElementById("categories");
     categoriesElement.replaceChildren();
     // getting totals
@@ -96,6 +79,7 @@ function updateCategories() {
             let siteElement = siteTemplate.cloneNode(true)
             categoryElement.appendChild(siteElement);
 
+            siteElement.getElementsByClassName("siteIcon")[0].src = site.icon;
             siteElement.getElementsByClassName("siteName")[0].innerHTML = site.name;
             siteElement.getElementsByClassName("siteAmount")[0].innerHTML = Math.round(site.amount * 100) / 100 + " kg";
             siteElement.getElementsByClassName("sitePercent")[0].innerHTML = "(" + Math.round(site.percentage * 100) / 100 + "%)";
@@ -124,7 +108,79 @@ function toggleCollapse(element) {
 }
 
 function onLoad() {
-    updateCategories();
+    loadCategories();
+    loadGraph();
+}
+
+async function loadCategories() {
+    const categories = [
+        {
+            name: "Streaming", percentage: 0, amount: 0, sites: [
+                {name: "Netflix", icon: "https://raw.githubusercontent.com/karlhadwen/netflix/master/public/favicon.ico", percentage: 0, amount: 42.4325},
+                {name: "Amazon Prime", icon: "https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png", percentage: 0, amount: 24.231742},
+                {name: "Disney+", percentage: 0, amount: 5.241}
+            ]
+        },
+        {
+            name: "Shopping", percentage: 0, amount: 0, sites: [
+                {name: "Amazon", icon: "https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png", percentage: 0, amount: 18.5235},
+                {name: "Ebay", percentage: 0, amount: 16.1245}
+            ]
+        },
+        {
+            name: "Nothing", percentage: 0, amount: 0, sites: []
+        }
+    ]; await sleep(2000); // TODO: replace when API
+
+    updateCategories(categories);
+}
+
+async function loadGraph() {
+    const carbonOverTime = [
+        {time: new Date("'December 17, 2022 03:24:00"), amount: 50},
+        {time: new Date("'December 17, 2022 04:00:00"), amount: 60},
+        {time: new Date("'December 17, 2022 05:00:00"), amount: 100},
+    ]; await sleep(5000); // TODO: replace when API
+
+    const xValues = [];
+    const yValues = [];
+    for (const carbonTime of carbonOverTime) {
+        xValues.push(carbonTime.time);
+        yValues.push(carbonTime.amount);
+    }
+
+    const config = {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "rgba(0,0,0,1.0)",
+                borderColor: "rgba(0,0,0,0.1)",
+                data: yValues
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {display: false}
+            },
+            scales: {
+                x: {
+                    type: 'time'
+                },
+                y: {
+                    min: 0
+                }
+            }
+        }
+    }
+
+    const myChart = new Chart("carbonPerTime", config);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 window.onload = onLoad;

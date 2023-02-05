2bed5f66ca4b50686611b0b3b203bdc361832f94
Timothy Langer <zeevox.dev@gmail.com>
2023-02-05 01:44:56 +0000

    Remove code duplication
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- client/getData.py
+++ client/getData.py
@@ -6,45 +6,35 @@ HOME = pathlib.Path(__file__).parent.absolute()
 v4_ranges = {}
 v6_ranges = {}
 
-services = {'amazon': 'aws', 'google': 'gcp', 'azure': 'azure'}
-ipv4_fmt = {
-    'amazon': 'ip_prefix',
-    'google': 'ipv4Prefix',
-    'azure': 'addressPrefix'
-}
-ipv6_fmt = {
-    'amazon': 'ipv6_prefix',
-    'google': 'ipv6Prefix',
-    'azure': 'addressPrefix'
-}
-scope_fmt = {'aws': 'region', 'gcp': 'scope', 'azure': ''}
+services = {"amazon": "aws", "google": "gcp", "azure": "azure"}
+ipv4_fmt = {"amazon": "ip_prefix", "google": "ipv4Prefix", "azure": "addressPrefix"}
+ipv6_fmt = {"amazon": "ipv6_prefix", "google": "ipv6Prefix", "azure": "addressPrefix"}
+scope_fmt = {"aws": "region", "gcp": "scope", "azure": ""}
 
 
 def get_co2(service, scope):
-    fileEnd = scope.replace('-', '_')
-    path = HOME / f'../assets/networking-emissions/{service}/{service}-{fileEnd}.json'
+    fileEnd = scope.replace("-", "_")
+    path = HOME / f"../assets/networking-emissions/{service}/{service}-{fileEnd}.json"
     if path.exists():
-        with open(
-                HOME /
-                f'../assets/networking-emissions/{service}/{service}-{fileEnd}.json',
-                'r') as file:
+        with open(path, "r") as file:
             data = json.load(file)
             return data["co2e"]
     else:
         return None
 
 
-for name in ['amazon', 'google']:
-    with open(HOME / f'../assets/ip-ranges/{name}-ip-ranges.json',
-              'r') as file:
+for name in ["amazon", "google"]:
+    with open(HOME / f"../assets/ip-ranges/{name}-ip-ranges.json", "r") as file:
         data = json.load(file)
         for prefix in tqdm(data["prefixes"] + data.get("ipv6_prefixes", [])):
             if ipv4_fmt[name] in prefix:
                 v4_ranges[prefix[ipv4_fmt[name]]] = get_co2(
-                    services[name], prefix[scope_fmt[services[name]]])
-            else:  #ipv6
+                    services[name], prefix[scope_fmt[services[name]]]
+                )
+            else:  # ipv6
                 v6_ranges[prefix[ipv6_fmt[name]]] = get_co2(
-                    services[name], prefix[scope_fmt[services[name]]])
+                    services[name], prefix[scope_fmt[services[name]]]
+                )
 
 # Map from the region naming scheme used by the Azure IPs JSON file from Microsoft to the region names used in our Climatiq dataset
 AZURE_MAP = {
@@ -112,14 +102,20 @@ with open(HOME / "../assets/ip-ranges/azure-ip-ranges.json", "r") as file:
         if not region:
             continue
         climatiq_region = AZURE_MAP[region]
-        v4_ranges.update({
-            prefix: get_co2("azure", climatiq_region)
-            for prefix in properties["addressPrefixes"] if "::" not in prefix
-        })
-        v6_ranges.update({
-            prefix: get_co2("azure", climatiq_region)
-            for prefix in properties["addressPrefixes"] if "::" in prefix
-        })
+        v4_ranges.update(
+            {
+                prefix: get_co2("azure", climatiq_region)
+                for prefix in properties["addressPrefixes"]
+                if "::" not in prefix
+            }
+        )
+        v6_ranges.update(
+            {
+                prefix: get_co2("azure", climatiq_region)
+                for prefix in properties["addressPrefixes"]
+                if "::" in prefix
+            }
+        )
 
-json.dump(v4_ranges, open(HOME / '../assets/all-ranges.json', 'w'))
-json.dump(v6_ranges, open(HOME / '../assets/v6-ranges.json', 'w'))
+json.dump(v4_ranges, open(HOME / "../assets/all-ranges.json", "w"))
+json.dump(v6_ranges, open(HOME / "../assets/v6-ranges.json", "w"))
